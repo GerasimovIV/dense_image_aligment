fixed gradient and coords arrays

dense_image_aligment/utils.py
@@ -15,21 +15,41 @@ def compute_image_grad(image: np.ndarray) -> np.ndarray:
         np.ndarray: matrix with shape N x 2 where N is a number of pixels in image
     """
 
-    gigx = np.zeros_like(image, dtype=image.dtype)
-    gigx[:, 1:-1] = (image[:, 2:] - image[:, :-2]) / 2
-    gigy = np.zeros_like(image, dtype=image.dtype)
-    gigy[1:-1] = (image[2:] - image[:-2]) / 2
+    dI_dx = np.zeros_like(image, dtype=image.dtype)
+    dI_dy = np.zeros_like(image, dtype=image.dtype)
+
+    dI_dx[:, 1:-1] = (image[:, 2:] - image[:, :-2]) / 2
+    dI_dx[:, 0] = (image[:, 1] - image[:, 0])
+    dI_dx[:, -1] = (image[:, -1] - image[:, -2])
+
+    dI_dy[1:-1, :] = (image[2:, :] - image[:-2, :]) / 2
+    dI_dx[0, :] = (image[1, :] - image[0, :])
+    dI_dx[-1, :] = (image[-1, :] - image[-2, :])
+
 
     nabla_I = np.vstack(
         [
-            gigx.reshape(-1),
-            gigy.reshape(-1),
+            dI_dx.reshape(-1),
+            dI_dy.reshape(-1),
         ]
     ).T
 
-    x_coord = np.arange(image.shape[1], dtype=np.float32)
-    y_coord = np.arange(image.shape[0], dtype=np.float32)
-    x_coord, y_coord = np.meshgrid(x_coord, y_coord, indexing='xy')
+    x_coord = np.repeat(
+        np.arange(
+            image.shape[1],
+            dtype=image.dtype
+        )[None, ...],
+        image.shape[0],
+        axis=0
+    )
+    y_coord = np.repeat(
+        np.arange(
+            image.shape[0],
+            dtype=image.dtype
+        )[..., None],
+        image.shape[1],
+        axis=1
+    )
 
     image_pixels_coordinates = np.vstack(
         [

draft.py
@@ -9,27 +9,30 @@ from dense_image_aligment import (
 )
 from dense_image_aligment.transformations.affine import AffineTransformation
 
-image = read_as_grayscale('./media/kanade_image.jpg')
-templ = read_as_grayscale('./media/kanade.jpg')
-
-image.shape, templ.shape
+image = read_as_grayscale('./media/kanade.jpg')
+templ = read_as_grayscale('./media/kanade_image.jpg')
 
 
 method, params = image_aligment_method(key='forward_additive')
-params['alpha'] = 1.
+params['alpha'] = 1.0
 params['max_iterations'] = 100
-params['p_init'] = np.array(
-    [
-        [1, 0.1, 40],
-        [0.2, 1., 70.],
-    ]
-).reshape(-1)
+params['p_init'] = np.array([[  1.02040816,  -0.10204082, -33.67346939],
+       [ -0.20408163,   1.02040816, -63.26530612]]).reshape(-1)
 
 affine_transform = AffineTransformation(params['p_init'].reshape(-1))
+
 
 ps = method(
     image=image,
     template=templ,
     coord_transform=affine_transform,
     **params
+)
+
+affine_transform.p = ps[-1]
+
+show_data(
+    image=image,
+    template=templ,
+    coords_transform=affine_transform
 )